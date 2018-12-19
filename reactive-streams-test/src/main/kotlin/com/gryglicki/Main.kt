00e867f010286b0284ee75061c10fb70d5b0d944
Michal Gryglicki <michal.gryglicki@allegro.pl>
2018-12-20 00:30:36 +0100

    Publisher without recursive problem

--- reactive-streams-test/src/main/kotlin/com/gryglicki/Main.kt
+++ reactive-streams-test/src/main/kotlin/com/gryglicki/Main.kt
@@ -1,5 +1,8 @@
 package com.gryglicki
 
+/**
+ * A single main class is needed by the gradle application plugin, so here it is ;)
+ */
 class Main {
     companion object {
         @JvmStatic fun main(args: Array<String>) {
--- reactive-streams-test/src/main/kotlin/com/gryglicki/rs/MyPublisher.kt
+++ reactive-streams-test/src/main/kotlin/com/gryglicki/rs/MyPublisher.kt
@@ -9,6 +9,10 @@ import java.util.*
 class MyPublisher(private var completeLimit: Long = Long.MAX_VALUE, private var errorLimit: Long = Long.MAX_VALUE) : Publisher<String>, Subscription {
     private val logger = KotlinLogging.logger {}
 
+    private var alreadySent: Long = 0L
+    private var requested: Long = 0L
+    private var insideRequest: Boolean = false
+
     private var subscriber: Subscriber<in String>? = null
     private var cancelled: Boolean = false
 
@@ -24,30 +28,35 @@ class MyPublisher(private var completeLimit: Long = Long.MAX_VALUE, private var 
     }
 
     /**
-     * UWAGA: publisher.request(n) i subscriber.onNext(elem) rekurencyjnie się nawzajem wywołują!!
      * Trzeba uważać, żeby:
      *  - zmniejszyć completeLimit i errorLimit przed zrobieniem onNext, bo inaczej będzie za dużo elementeów
      *  - przypisać subscriber=null i sprawdzać to, przed wywołaniem onComplete() i onError(), bo inaczej będzie za dużo wywołań
      */
     override fun request(n: Long) {
-        logger.info { "request($n): I was requested for more data" }
-        for (i in 1..n) {
-            logger.debug { "!!!!!!!!!!!!!! i=$i, cancelled=$cancelled, completeLimit=$completeLimit, errorLimit=$errorLimit" }
-            if (!cancelled && completeLimit > 0L && errorLimit > 0L) {
-                completeLimit--
-                errorLimit--
-                subscriber?.onNext(generateMoreData())
-            } else break
-        }
-        if (completeLimit == 0L && subscriber != null) {
-            logger.info { "stream reached the end" }
-            subscriber?.onComplete()
-            subscriber = null
-        }
-        if (errorLimit == 0L && subscriber != null) {
-            logger.info { "stream encounter an error" }
-            subscriber?.onError(IllegalStateException("Stream exception"))
-            subscriber = null
+        requested += n
+        if (! insideRequest) {
+            insideRequest = true
+            logger.info { "request($n): I was requested for more data" }
+            while (alreadySent < requested) {
+                logger.debug { "!!!!!!!!!!!!!! requested=$requested, alreadySent=$alreadySent, cancelled=$cancelled, completeLimit=$completeLimit, errorLimit=$errorLimit" }
+                if (!cancelled && completeLimit > 0L && errorLimit > 0L) {
+                    completeLimit--
+                    errorLimit--
+                    subscriber?.onNext(generateMoreData())
+                } else break
+                alreadySent++
+            }
+            if (completeLimit == 0L && subscriber != null) {
+                logger.info { "stream reached the end" }
+                subscriber?.onComplete()
+                subscriber = null
+            }
+            if (errorLimit == 0L && subscriber != null) {
+                logger.info { "stream encounter an error" }
+                subscriber?.onError(IllegalStateException("Stream exception"))
+                subscriber = null
+            }
+            insideRequest = false
         }
     }
 
